623771c2d79e45af780fff870ad960ccd76b7ea8
stef boy <stef@exemple.com>
2022-07-29 17:25:07 +0200

    add go command

--- main.go
+++ main.go
@@ -64,6 +64,7 @@ func main(){
 	router.Run("localhost:9090")
 }
 
+// lancer la commande go => < go run .>
 //  curl http://localhost:9090/albums --header "Content-Type: application/json" --request "GET"
 //  curl http://localhost:9090/albums/2 --header "Content-Type: application/json" --request "GET"
 //  curl http://localhost:9090/albums --include --header "Content-Type: application/json" --request "POST" --data '{"id": "4","title":"My title songs","artist":"Jhonny Haaliday","price":56.36}'
